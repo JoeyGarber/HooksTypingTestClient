af947136620cef9b5aec18fd49b7bdeba2c8f760
joeygarber <joseph.z.garber@gmail.com>
2022-08-30 13:28:57 -0400

    Started setting up userTests in Tests route

--- src/routes/auth/sign-in.jsx
+++ src/routes/auth/sign-in.jsx
@@ -31,7 +31,7 @@ function SignIn () {
 
     <div className='sign'>
       <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-        <h4>Sign Up</h4>
+        <h4>Sign In</h4>
         <Form onSubmit={onSignIn}>
           <Form.Group controlId='email'>
             <Form.Label>Email address</Form.Label>
--- src/routes/tests.jsx
+++ src/routes/tests.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react'
 import { NavLink, Outlet, useSearchParams } from 'react-router-dom'
 import Dropdown from 'react-bootstrap/Dropdown'
 import getTests from '../data'
+import { indexTests } from '../api/tests'
+import { useAuth } from '../contexts/authProvider'
 
 export default function Tests() {
-  let tests = getTests()
-  let [searchParams, setSearchParams] = useSearchParams()
+  const [userTests, setUserTests] = useState([])
+  const tests = getTests()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const { user } = useAuth()
+
+  useEffect(() => {
+    if (user) {
+      indexTests(user)
+      .then(response => setUserTests(response.data.tests))
+      .catch(error => console.log(error))
+    }
+  }, [])
+
 
   return (
     <>
@@ -18,9 +32,13 @@ export default function Tests() {
       {tests.map((test) => (
           <Dropdown.Item key={test._id} href={`/tests/${test._id}`}>{test.title}</Dropdown.Item>
         ))}
+      {userTests.map((test) => (
+        <Dropdown.Item key={test._id} href={`/tests/${test._id}`}>{test.title}</Dropdown.Item>
+      ))}
       </Dropdown.Menu>
     </Dropdown>
     <Outlet />
+    <button onClick={() => console.log(userTests)}>userTests</button>
     </>
   )
 }
