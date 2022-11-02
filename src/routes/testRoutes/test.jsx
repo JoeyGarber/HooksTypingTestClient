58da873af11ffeb64b8401e6d4249762132884d7
joeygarber <joseph.z.garber@gmail.com>
2022-11-02 16:21:53 -0400

    Timer stops when user reaches end of text

--- src/routes/testRoutes/test.jsx
+++ src/routes/testRoutes/test.jsx
@@ -1,20 +1,21 @@
 import { useParams } from "react-router-dom"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { showTest } from "../../api/tests"
 import { useAuth } from "../../contexts/authProvider"
 
 export default function Test () {
-  const SECONDS = 60
+  const SECONDS = 10
 
   const [test, setTest] = useState(null)
   const [correct, setCorrect] = useState(0)
   const [incorrect, setIncorrect] = useState(0)
   const [charIndex, setCharIndex] = useState(0)
   const [countDown, setCountDown] = useState(SECONDS)
-  const [status, setStatus] = useState('waiting')
+  const [timerRunning, setTimerRunning] = useState(false)
 
   const params = useParams()
   const { user }  = useAuth()
+  const Ref = useRef()
 
   // Get the test when this component renders, as denoted by when the params.testId changes
   useEffect(() => {
@@ -24,24 +25,25 @@ export default function Test () {
       })
   }, [params.testId, user])
 
+  useEffect(() => {
+    if (timerRunning === false) {
+      clearInterval(Ref.current)
+    }
+  }, [timerRunning])
+
   const start = () => {
-    if (status === 'finished') {
-      setStatus()
-    }
-    if (status !== 'started') {
-      setStatus('started')
-      let interval = setInterval(() => {
-        setCountDown((prevCountdown) => {
-          if (prevCountdown === 0) {
-            clearInterval(interval)
-            setStatus('finished')
-            return SECONDS
-          } else {
-            return prevCountdown - 1
-          }
-        })
-      }, 1000)
-    }
+    setTimerRunning(true)
+    Ref.current = setInterval(() => {
+      setCountDown((prevCountdown) => {
+        if (prevCountdown === 0) {
+          clearInterval(Ref.current)
+          setTimerRunning(false)
+          return SECONDS
+        } else {
+          return prevCountdown - 1
+        }
+      })
+    }, 1000)
   }
 
   const checkMatch = (event) => {
@@ -58,15 +60,15 @@ export default function Test () {
       }
       setCharIndex(charIndex + 1)
     } else if (event.key === 'Backspace' && charIndex > 0) {
-      console.log(characters[charIndex-1].className)
-
-      if (characters[charIndex-1].className === 'correct') {
+      if (characters[charIndex - 1].className === 'correct') {
         setCorrect(correct - 1)
       } else if (characters[charIndex - 1].className === 'incorrect') {
         setIncorrect(incorrect - 1)
       }
       characters[charIndex - 1].classList.remove('correct', 'incorrect')
       setCharIndex(charIndex - 1)
+    } else if (!characters[charIndex]) {
+      setTimerRunning(false)
     }
 }
 
@@ -83,12 +85,12 @@ export default function Test () {
               })}
             </p>
         </div>
-        <input type='text' className='input' disabled={status !== 'started'} onKeyDown={checkMatch}/>
+        <input type='text' className='input' disabled={timerRunning !== true} onKeyDown={checkMatch}/>
       </div>
       <div className='section'>
         <div className='column'>
           <p>Words per minute (one word = 5 chars):</p>
-          <p>{(correct * 12) / (60 - countDown)}</p>
+          <p>{Math.round((correct * 12) / (SECONDS - countDown), 2)}</p>
         </div>
         <div>
           <button className='start' onClick={start}>Start</button>
